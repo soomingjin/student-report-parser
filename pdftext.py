73398bc8d4148615f8eb08644b5408bb45553ccf
randName <kwokshungit@gmail.com>
2014-01-19 08:51:25 +0800

    added some testing ability

--- pdftext.py
+++ pdftext.py
@@ -3,3 +3,13 @@
 from subprocess import check_output
 
 def get( filename ): return check_output( [ "ps2ascii", filename ] )
+
+if __name__ == "__main__":
+	import sys, getopt
+
+	try:
+		( opts, args ) = getopt.getopt( sys.argv[1:], 'd' )
+	except getopt.GetoptError:
+		pass
+
+	for filename in args: print get( filename )

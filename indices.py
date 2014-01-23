003909c057abd742532093aca7033c21ace8252a
randName <kwokshungit@gmail.com>
2014-01-23 20:09:24 +0800

    sorted out recursive traversal

--- indices.py
+++ indices.py
@@ -4,7 +4,6 @@ import re, yaml
 
 def read_config():
 	from os import listdir
-
 	idir = 'indices'
 	idxs = {}
 
@@ -19,34 +18,31 @@ def search( needle, haystack ):
 	except AttributeError:
 		return ''
 
-def extract( haystack, needles, pre, post ):
-	if isinstance( needles, dict ):
-		return { k: [ search( pre+t+post, haystack ) for t in x ] for k,x in needles.iteritems() } 
+def walk( d, c ):
+	if 'indices' in d:
+		r = d['regex']
+
+		for i in d['indices']:
+			for ix in i.keys():
+				if 'sub' in r: ix = re.sub( r.get('sub'), r.get('repl'), ix )
+				v = search( r.get('pre','')+ix+r.get('post',''), c ) 
+				i.values()[0].update( { 'value': v } )
 	else:
-		return [ search( pre+t+post, haystack ) for t in needles ]
+		if isinstance( d, dict ):
+			for k,x in d.items():
+				x = walk( x, c )
+	return d
 
 def parse_indices( pages ):
 
 	indices = read_config()
-	headers = indices.keys()
 
-	page = [ [ i for i, s in enumerate(pages) if k in s ] for k in headers  ]
+	for k,idx in indices.items():
+		p = [ i for i, s in enumerate(pages) if k in s ]
+		compositepage = re.sub( r'.*%s.*' % (k,) , '', ''.join([ pages[x] for x in p ]) )
+		compositepage = ' '.join( re.sub( r'\n+\d\n+', r'', compositepage ).split() )
 
-	for p in page:
-		compositepage = re.sub( r'.*.*' , '', ''.join([ pages[x] for x in p ]) )
+		print walk( idx, compositepage )
 
-	return False
-
-	if page[1]:
-		pf_page = ''.join([ pages[x] for x in page[1] ])
-		c_profile = extract( pf_page, composite_indices, '', r' (1?\d.\d)' )
-
-	if page[3]:
-		verbose_report = ' '.join( re.sub( r'\s\s\d\s\s', r'', compositepage ).split() )
-
-		ci = { k: [ re.sub(' ',' ?',t) for t in x ] for k,x in criterion_indices.iteritems() }
-		criterion_scores = extract( verbose_report, ci, '', r'.*?\((1?\d)\).' )
-
-	return ( b_profile, c_profile, criterion_scores )
 
 if __name__ == "__main__": pass
